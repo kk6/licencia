d3e053d620533effddc575217596f6a4496bbcb6
kk6 <hiro.ashiya@gmail.com>
2019-01-17 23:53:28 +0900

    Add option "order" for list command.

--- licencia/commands.py
+++ licencia/commands.py
@@ -13,6 +13,7 @@ class ListCommand(Command):
     List the licenses for external packages used in your repository.
 
     list
+        {--o|order=name : Control display order by "name" or "license".}
     """
 
     def handle(self):
@@ -28,6 +29,9 @@ class ListCommand(Command):
             sys.exit(1)
 
         results = search_packages_info(package_names)
+        if self.option("order") == "license":
+            results = sorted(results, key=lambda p: p["license"])
+
         rows = [(p["name"], p["license"]) for p in results]
         table = create_table(rows)
         print(table)
